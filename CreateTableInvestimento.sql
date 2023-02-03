596da35a181cb0e93cdf1fdf9bd7cd6ef3ee39e8
maransi <maransi>
2023-02-03 13:46:14 -0300

    Create table

--- CreateTableInvestimento.sql
+++ CreateTableInvestimento.sql
@@ -22,10 +22,13 @@ CREATE TABLE investimento(      id                  INT             UNSIGNED NOT
                                 idEmissor           INT             NOT NULL,
                                 idCategoriaPapel    INT             NOT NULL,
                                 idIndexador         INT             NOT NULL,
+                                liquidezDiaria      CHAR(1)         NOT NULL DEFAULT 'N',
                                 percentualRemuneracao DOUBLE(9,2)   DEFAULT 0,
+                                dataResgate         DATE,
                                 dataAplicacao       DATE            NOT NULL,
                                 valorAplicado       DECIMAL(15,2)   DEFAULT 0,
-                                valorAtualizado     DECIMAL(15,2)   DEFAULT 0 );
+                                valorAtualizado     DECIMAL(15,2)   DEFAULT 0,
+                                observacao          VARCHAR(250) );
 
 CREATE TABLE investimentoPosicao( id                INT             UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
                                 idInvestimento      INT             NOT NULL,
@@ -33,3 +36,36 @@ CREATE TABLE investimentoPosicao( id                INT             UNSIGNED NOT
                                 valorAtualizado     DECIMAL(15,2)   NOT NULL,
                                 observacao          VARCHAR(250));
 
+
+INSERT INTO emissor( nome )
+VALUES( 'BANCO MASTER'),
+        ('SICRED'),
+        ('BMG'),
+        ('BTG PACTUAL'),
+        ('BANCO DAYCOVAL'),
+        ('BANCO PARANA'),
+        ('BANCO ABC');
+
+
+
+INSERT INTO categoriaPapel( nome )
+VALUES( 'CDB'),
+        ('LC'),
+        ('LCI'),
+        ('LCA'),
+        ('DEBENTURES'),
+        ('TESOURO DIRETO');
+
+INSERT INTO indexador( nome )
+VALUES('CDI'),
+        ('IPCA'),
+        ('PRE'),
+        ('SELIC');
+
+INSERT INTO investidor( nome )
+VALUES('MARCO ANTONIO DA SILVA');
+
+INSERT INTO distribuidor( nome )
+VALUES('NUINVEST'),
+        ('MODALMAIS'),
+        ('BANCO INTER');
