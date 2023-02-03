788e3f9e3440f9be8366ab78aa422953fc808e46
maransi <maransi>
2023-02-03 13:09:31 -0300

    CreateTable

--- CreateTableInvestimento.sql
+++ CreateTableInvestimento.sql
@@ -4,14 +4,14 @@ CREATE TABLE emissor(           id                  INT             UNSIGNED NOT
 CREATE TABLE categoriaPapel(    id                  INT             UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
                                 nome                VARCHAR(100)   NOT NULL);
 
-CREATE TABLE indexador(         id                  INT             UNSIGNED NOT NULL       AUTO_IMCREMENT PRIMARY KEY,
+CREATE TABLE indexador(         id                  INT             UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
                                 descricao           VARCHAR(50)     NOT NULL);
 
 CREATE TABLE investidor(        id                  INT             UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
                                 nome                VARCHAR(50)     NOT NULL);
 
-CREATE TABLE distribuidor(      id                  IN              UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
-                                nome                VARCHAR(50)     VARCHAR(50) );
+CREATE TABLE distribuidor(      id                  INT             UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
+                                nome                VARCHAR(50)     NOT NULL );
 
 CREATE TABLE contaInvestimento( id                  INT             UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
                                 idDistribuidor      INT             UNSIGNED NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE contaInvestimento( id                  INT             UNSIGNED NOT
 
 CREATE TABLE investimento(      id                  INT             UNSIGNED NOT NULL       AUTO_INCREMENT PRIMARY KEY,
                                 idContaInvestimento INT             NOT NULL,
-                                idEmissor           INT             NOT NULL
+                                idEmissor           INT             NOT NULL,
                                 idCategoriaPapel    INT             NOT NULL,
                                 idIndexador         INT             NOT NULL,
                                 percentualRemuneracao DOUBLE(9,2)   DEFAULT 0,
